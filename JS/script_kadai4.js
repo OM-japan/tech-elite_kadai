e2c2a7d33b24d65457eafd22e14f568612d0be4a
OM-japan <ogawama@freeeng.net>
2025-05-12 15:47:45 +0900

    Update script_kadai4.js

--- JS/script_kadai4.js
+++ JS/script_kadai4.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 
 $("body").on("click" , ".header_title, .header_menu" , function(){// クリックでスクロール移動する
     console.log("クリックが認識されました")
+    debugger
     const scroll_target = $(this).find("a").attr("href");
     const current_page = window.location.pathname;// 現在のページのURLのパスを取得
     if (scroll_target && scroll_target.startsWith(current_page)) {
