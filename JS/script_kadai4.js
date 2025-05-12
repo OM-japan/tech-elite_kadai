b1be078898f405a705e3845a74bebb0ecd242f21
OM-japan <ogawama@freeeng.net>
2025-05-12 13:39:59 +0900

    Update script_kadai4.js

--- JS/script_kadai4.js
+++ JS/script_kadai4.js
@@ -11,11 +11,10 @@ $(document).ready(function(){
 
 $("body").on("click" , ".header_title, .header_menu" , function(){// クリックでスクロール移動する
     console.log("クリックが認識されました")
-    debugger;
-    const scroll_target = $(this).find("a").attr("href");
-    const target_id = scroll_target.split("#")[1];
-    const scroll_position = $("#" + target_id).offset().top - get_header_height();
-    $("html, body").animate({ scrollTop: scroll_position }, 400);
+    // const scroll_target = $(this).find("a").attr("href");
+    // const target_id = scroll_target.split("#")[1];
+    // const scroll_position = $("#" + target_id).offset().top - get_header_height();
+    // $("html, body").animate({ scrollTop: scroll_position }, 400);
 });
 
 $("body").on("click" , ".hamburger" , function(){//ハンバーガーメニューの表示折りたたみ
