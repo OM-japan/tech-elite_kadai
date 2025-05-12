fd7d6324595f9313c08f2bc779cc2ef932c35da6
OM-japan <ogawama@freeeng.net>
2025-05-12 12:49:00 +0900

    Update script_kadai4.js

--- JS/script_kadai4.js
+++ JS/script_kadai4.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 $("body").on("click" , ".header_title, .header_menu" , function(){// クリックでスクロール移動する
     console.log("クリックが認識されました")
     const scroll_target = $(this).find("a").attr("href");
-    const scroll_position = $(scroll_target).offset().top ;
+    const scroll_position = $(scroll_target).offset().top - get_header_height();
     $("html, body").animate({ scrollTop: scroll_position }, 400);
 });
 
