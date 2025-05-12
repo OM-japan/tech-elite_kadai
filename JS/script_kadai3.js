fd76cecdb5d16b6df7886d5172be64d313a783a0
OM-japan <ogawama@freeeng.net>
2025-05-12 09:45:26 +0900

    upload

---
+++ JS/script_kadai3.js
@@ -0,0 +1,42 @@
+function get_header_height(){// ヘッダーのheight値を取得する
+    return $(".header_list").outerHeight();
+}
+
+
+$(document).ready(function(){
+  $("#header").load("header.html");
+});
+
+
+$(".header_title, .header_menu").click(function(){// クリックでスクロール移動する
+    const scroll_target = $(this).find("a").attr("href");
+    const scroll_position = $(scroll_target).offset().top - get_header_height();
+    $("html, body").animate({ scrollTop: scroll_position }, 400);
+});
+
+$(".service_menu li").hover(
+    function () {// マウスが乗ったときの処理
+      $(this).addClass("active");
+    },
+    function () {// マウスが離れたときの処理
+      const className = $(this).attr("class").split(" active")[0];
+      const idSelector = "#" + className;
+      if (!$(idSelector).hasClass("active")) {//service_descriptionがidのクラスがactiveでない場合
+        $(this).removeClass("active");
+      }
+    }
+  );
+
+$(".service_menu li").click(function(){//サービス内容をクリックすると説明が表示される
+    $(".service_menu li").removeClass("active")
+    $(".service_description p").removeClass("active")
+    const service_target_class = $(this).attr("class").split(" active")[0];
+    const description_target_id = "#" + service_target_class
+    $(description_target_id).addClass("active");
+    $(this).addClass("active");
+});
+
+$("#hamburger").click(function(){//ハンバーガーメニューの表示折りたたみ
+  $("#hamburger").toggleClass("open");
+  $(".header_menu").slideToggle();
+});
