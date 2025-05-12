b495e6776333b3b748df864fb551420868a61eac
OM-japan <ogawama@freeeng.net>
2025-05-12 11:14:57 +0900

    Update script_kadai4.js

--- JS/script_kadai4.js
+++ JS/script_kadai4.js
@@ -4,22 +4,14 @@ function get_header_height(){// ヘッダーのheight値を取得する
 
 
 $(document).ready(function(){
-  $("#header").load("header.html", function() {
-    console.log("Document is ready.");
-
-    $(".header_title, .header_menu").on("click", function() {
-      console.log("move is ready.");
-      // const scroll_target = $(this).find("a").attr("href");
-      // const scroll_position = $(scroll_target).offset().top - get_header_height();
-      // console.log(scroll_position);
-      // $("html, body").animate({ scrollTop: scroll_position }, 400);
-    });
-  });
+  $("#header").load("header.html");
 });
 
-$("#hamburger").click(function(){//ハンバーガーメニューの表示折りたたみ
-  $("#hamburger").toggleClass("open");
-  $(".header_menu").slideToggle();
+
+$(".header_title, .header_menu").click(function(){// クリックでスクロール移動する
+    const scroll_target = $(this).find("a").attr("href");
+    const scroll_position = $(scroll_target).offset().top - get_header_height();
+    $("html, body").animate({ scrollTop: scroll_position }, 400);
 });
 
 $(".service_menu li").hover(
@@ -36,12 +28,15 @@ $(".service_menu li").hover(
   );
 
 $(".service_menu li").click(function(){//サービス内容をクリックすると説明が表示される
-    console.log("クリックされました"); // クリックが発生しているか確認
     $(".service_menu li").removeClass("active")
     $(".service_description p").removeClass("active")
     const service_target_class = $(this).attr("class").split(" active")[0];
     const description_target_id = "#" + service_target_class
     $(description_target_id).addClass("active");
     $(this).addClass("active");
-    console.log("activeにしたぜ")
+});
+
+$("#hamburger").click(function(){//ハンバーガーメニューの表示折りたたみ
+  $("#hamburger").toggleClass("open");
+  $(".header_menu").slideToggle();
 });
